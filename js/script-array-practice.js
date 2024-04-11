ac1af863e54bb189e00aa1e81a5fa5d24b2001de
at143 <anshulkumar@yahoo.com>
2024-04-11 14:07:18 -0400

    after step 4

--- js/script-array-practice.js
+++ js/script-array-practice.js
@@ -1,12 +1,52 @@
 //STEP 1
+// let anshulFavoriteMovies = [
+//     "In Bruges",
+//     "The Accountant",
+//     "3 Idiots",
+//     "Parasite",
+//     "M*A*S*H"
+// ]
+
+// console.log(anshulFavoriteMovies[1])
 
 //STEP 2
+// let movies = new Array(length)
+// movies[0] = "In Bruges"
+// movies[1] = "The Accountant"
+// movies[2] = "3 Idiots"
+// movies[3] = "Parasite"
+// movies[4] = "M*A*S*H"
+
+// console.log(movies[0])
 
 //STEP 3
+// let movies = new Array(length)
+// movies[0] = "In Bruges"
+// movies[1] = "The Accountant"
+// movies[2] = "3 Idiots"
+// movies[3] = "Parasite"
+// movies[4] = "M*A*S*H"
+
+// movies.splice(2,0,"The Great Escape")
+// console.log(movies.length)
+// // console.log(movies.join("\n"))
+
 
 //STEP 4
+// let movies = []
+// movies[0] = "In Bruges"
+// movies[1] = "The Accountant"
+// movies[2] = "3 Idiots"
+// movies[3] = "Parasite"
+// movies[4] = "M*A*S*H"
+
+// delete movies[0]
+
+// console.log(movies.join("\n"))
+
 
 //STEP 5
+
 
 //STEP 6
 
